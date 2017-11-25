c78b3a171e5ce1ae42b05df416fb091e88b84c27
Kopninsky <kopninsky@gmail.com>
2017-11-25 16:56:11 +0300

    pre final js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,71 @@
-/*---------Hamburger Menu----------*/
-document.getElementById('hamburger__nav')
-/*---------Hamburger Menu----------*/
+/*----------------------------------------------------------------------------------Some Functions----------*/
+let toggleFunc = function (className, activeTrigger){
+  className.on('click', (e) =>{
+  e.preventDefault();
+  let item = $(e.currentTarget);
+  item.toggleClass(activeTrigger).siblings().removeClass(activeTrigger);
+  });};
+let hidenbarFunc = function (sectionTriggerd, sectionWhere, activeTrigger){
+  sectionTriggerd.on('click', (e) =>{
+  e.preventDefault();
+  sectionWhere.toggleClass(activeTrigger);
+  });};
+/*-----------Page Scroll----------*/
 
-/*---------Hamburger Menu----------*/
+/*----------------------------------------------------------------------------------Hamburger Menu----------*/
+$('.hamburger__nav').on('click', (e) =>{
+  e.preventDefault();
+  $('.mobile-menu').toggleClass('mobile-menu--is-active');
+  $('.hamburger__nav').toggleClass('hamburger__nav--is-active');
+});
+/*----------------------------------------------------------------------------Hiden Igredients Bar----------*/
+hidenbarFunc($('.burger__options--ingredient'),$('.burger__options--hidenbar'),'burger__options--hidenbar--is-active');
+hidenbarFunc( $('.exit__btn'),$('.burger__options--hidenbar'),'burger__options--hidenbar--is-active');
+/*---------Burgers Menu Slider----------*/
+$('.arrow__next').on('click', (e) =>{
+  e.preventDefault();
+  let itemList = $('.burger__slider'),
+      items = itemList.find('.slider__item'),
+      activeItem = items.filter('.slider__item--is-active'),
+      reqItem = activeItem.next(),
+      reqIndex = reqItem.index(),
+      duration = 400;
+  if(reqItem.length){
+    itemList.animate({
+        'left' : -reqIndex * 100 + '%'
+    }, duration, function() {
+      activeItem.removeClass('slider__item--is-active');
+      reqItem.addClass('slider__item--is-active');
+    })
+  }
+});
 
-/*---------Hamburger Menu----------*/
+$('.arrow__prev').on('click', (e) =>{
+  e.preventDefault();
+  let itemList = $('.burger__slider'),
+      items = itemList.find('.slider__item'),
+      activeItem = items.filter('.slider__item--is-active'),
+      reqItem = activeItem.prev(),
+      reqIndex = reqItem.index(),
+      duration = 400;
+  if(reqItem.length){
+    itemList.animate({
+        'left' : -reqIndex * 100 + '%'
+    }, duration, function() {
+      activeItem.removeClass('slider__item--is-active');
+      reqItem.addClass('slider__item--is-active');
+    })
+  }
+});
+/*-------------------------------------------------------------------------------------Team Toggle----------*/
+toggleFunc($('.team__section'),'team__section--active');
+/*-------------------------------------------------------------------------------------Menu Toggle----------*/
+toggleFunc($('.menu__accordeon-item'),'item--is-activ');
+/*---------Delivery Modal----------*/
 
-/*---------Hamburger Menu----------*/
+/*---------Map API----------*/
 
-/*---------Hamburger Menu----------*/
 
-/*---------Hamburger Menu----------*/
 
-/*---------Hamburger Menu----------*/
 
-/*---------Hamburger Menu----------*/
 
-/*---------Hamburger Menu----------*/
