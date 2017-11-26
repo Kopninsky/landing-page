fa8796484b333516adc2a797aefe18fec466d002
Kopninsky <kopninsky@gmail.com>
2017-11-26 15:56:58 +0300

    prepre final

--- js/main.js
+++ js/main.js
@@ -1,28 +1,117 @@
 /*----------------------------------------------------------------------------------Some Functions----------*/
-let toggleFunc = function (className, activeTrigger){
-  className.on('click', (e) =>{
+let toggleFunc = (className, activeTrigger) => {
+  className.on('click touchstart', (e) =>{
   e.preventDefault();
   let item = $(e.currentTarget);
   item.toggleClass(activeTrigger).siblings().removeClass(activeTrigger);
-  });};
-let hidenbarFunc = function (sectionTriggerd, sectionWhere, activeTrigger){
-  sectionTriggerd.on('click', (e) =>{
+});};
+let hidenbarFunc = (sectionTriggerd, sectionWhere, activeTrigger) => {
+  sectionTriggerd.on('click touchstart', (e) =>{
   e.preventDefault();
   sectionWhere.toggleClass(activeTrigger);
-  });};
-/*-----------Page Scroll----------*/
+});};
+let defineSections = sections =>{
+  let activeSection = sections.filter('.active');
+  return {
+    activeSection: activeSection,
+    nextSection: activeSection.next(),
+    prevSection: activeSection.prev()
+  }
+};
+let switchMenuActiveClass = sectionEq => {
+  $('.aside__item').eq(sectionEq).addClass('aside__item--active').siblings().removeClass('aside__item--active');
+};
+let performTransition = sectionEq =>{
+  let position = sectionEq * -100 + '%';
+
+  if(inScroll) return
+  inScroll = true
+  
+  display.css({
+    'transform' : `translateY(${position})`,
+    '-webkit-transform' : `translateY(${position})`
+  });
+  sections.eq(sectionEq).addClass('active').siblings().removeClass('active');
+  setTimeout(()=>{
+  inScroll = false;
+  }, 1500);
+  setTimeout(()=>{
+    switchMenuActiveClass(sectionEq)
+    }, 500);
+};
+let scrollToSection = direction =>{
+  let section = defineSections(sections);
+  if(inScroll) return;
+  if(direction == 'up' && section.nextSection.length){
+    performTransition(section.nextSection.index())
+  };
+  if(direction == 'down' && section.prevSection.length){
+    performTransition(section.prevSection.index())
+  }
+
+};
+/*-----------Page Scroll-----------*/
+let display = $('.sitecontent');
+let sections = $('.section');
+let inScroll = false;
+let md = new MobileDetect(window.navigator.userAgent);
+let isMobile = md.mobile();
+
+
+$('.wrapper').on({
+  wheel : e => {
+    let deltaY = e.originalEvent.deltaY;
+    let direction = (deltaY > 0)?'up':'down';
+    scrollToSection(direction);
+  },
+  touchmove : e => (e.preventDefault())
+} );
+
+
+$(document).on('keydown', (e) =>{
+  let section = defineSections(sections);
+  if(inScroll) return;
+  switch(e.keyCode){
+    case 40 :
+    if(!section.nextSection.length) return;
+    performTransition(section.nextSection.index());
+    break;
+    case 38 :
+    if(!section.prevSection.length) return;
+    performTransition(section.prevSection.index());
+    break;
+  }
+})
+
+
+$('[data-scroll-to]').on('click touchstart', (e) =>{
+  e.preventDefault();
+  let elem = $(e.currentTarget);
+  let elemIndex = parseInt(elem.attr('data-scroll-to'));
+  performTransition(elemIndex);
+});
+if(isMobile){
+  $(window).swipe( {
+    swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+    scrollToSection(direction); 
+    }
+  });
+}
 
 /*----------------------------------------------------------------------------------Hamburger Menu----------*/
-$('.hamburger__nav').on('click', (e) =>{
+$('.hamburger__nav').on('click touchstart', (e) =>{
   e.preventDefault();
   $('.mobile-menu').toggleClass('mobile-menu--is-active');
   $('.hamburger__nav').toggleClass('hamburger__nav--is-active');
-});
+  });
 /*----------------------------------------------------------------------------Hiden Igredients Bar----------*/
 hidenbarFunc($('.burger__options--ingredient'),$('.burger__options--hidenbar'),'burger__options--hidenbar--is-active');
 hidenbarFunc( $('.exit__btn'),$('.burger__options--hidenbar'),'burger__options--hidenbar--is-active');
-/*---------Burgers Menu Slider----------*/
-$('.arrow__next').on('click', (e) =>{
+/*-----------------------------------------------------------------------------Burgers Menu Slider----------*/
+
+
+
+$('.arrow__next').on('click touchstart', (e) =>{
   e.preventDefault();
   let itemList = $('.burger__slider'),
       items = itemList.find('.slider__item'),
@@ -40,7 +129,7 @@ $('.arrow__next').on('click', (e) =>{
   }
 });
 
-$('.arrow__prev').on('click', (e) =>{
+$('.arrow__prev').on('click touchstart', (e) =>{
   e.preventDefault();
   let itemList = $('.burger__slider'),
       items = itemList.find('.slider__item'),
@@ -57,14 +146,79 @@ $('.arrow__prev').on('click', (e) =>{
     })
   }
 });
+
 /*-------------------------------------------------------------------------------------Team Toggle----------*/
 toggleFunc($('.team__section'),'team__section--active');
 /*-------------------------------------------------------------------------------------Menu Toggle----------*/
 toggleFunc($('.menu__accordeon-item'),'item--is-activ');
 /*---------Delivery Modal----------*/
 
-/*---------Map API----------*/
+/*-----------------------------------------------------------------------------------------Map API----------*/
+ymaps.ready(init);
+var myMap, 
+    myPlacemark;
 
+
+var placemarks = [ // Массив объектов который мы перебираем методом forEach
+  {
+    latitude: 55.75, 
+    longitude: 37.62,
+    hintContent: '<div class="map__hint">Брестская ул. д. 14</div>',
+    balloonContent: [
+      '<div class="map__balloon">',
+      '<img class="map__burger-img" src="../img/ymap/burger.png" alt="Бургер"/>',
+      "Самые вкусные бургеры, заходите в гости",
+      "</div>"
+    ]
+  },
+  {
+    latitude: 55.76, 
+    longitude: 37.63,
+    hintContent: '<div class="map__hint">Брестская ул. д. 14</div>',
+    balloonContent: [
+      '<div class="map__balloon">',
+      '<img class="map__burger-img" src="../img/ymap/burger.png" alt="Бургер"/>',
+      "Самые вкусные бургеры, заходите в гости",
+      "</div>"
+    ]
+  },
+  {
+    latitude: 55.74, 
+    longitude: 37.60,
+    hintContent: '<div class="map__hint">Брестская ул. д. 14</div>',
+    balloonContent: [
+      '<div class="map__balloon">',
+      '<img class="map__burger-img" src="../img/ymap/burger.png" alt="Бургер"/>',
+      "Самые вкусные бургеры, заходите в гости",
+      "</div>"
+    ]
+  }
+];
+
+/*Инициализация функции создания карты и добавление на карту placemark*/
+function init() {
+  var map = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 13,
+    controls: ["zoomControl"], // Выводим только кнопки зума
+    behaviors: ["drag"]
+  });
+  placemarks.forEach(function(item){
+    var placemark = new ymaps.Placemark(
+        [item.latitude, item.longitude],
+        {
+          hintContent: item.hintContent,
+          balloonContent: item.balloonContent.join("")
+        },
+        {
+          iconLayout: "default#image",
+          iconImageHref: "./img/ymap/map-marker.png",
+          iconImageSize: [46, 57],
+          iconImageOffset: [-23, -57]
+        });
+        map.geoObjects.add(placemark);
+  })
+} 
 
 
 
